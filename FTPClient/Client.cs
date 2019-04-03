f939855d32f815cf46aa17924e79bb2b0852ed8f
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2019-04-03 12:03:56 +0300

    Add project files.

---
+++ FTPClient/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPClient
+{
+    class Client
+    {
+        private string _Host = "10.7.180.107:21";
+        private string _UserName = "test_user";
+        private string _Password = "1234567890";
+
+        //объект для запроса данных
+        FtpWebRequest ftpRequest;
+
+        //объект для получения данных
+        FtpWebResponse ftpResponse;
+
+        //флаг использования SSL
+        private bool _UseSSL = false;
+        public FileStruct[] ListDirectory(string path)
+        {
+            if (path == null || path == "")
+            {
+                path = "/";
+            }
+            //Создаем объект запроса
+            ftpRequest = (FtpWebRequest)WebRequest.Create("ftp://" + _Host + path);
+            //логин и пароль
+            ftpRequest.Credentials = new NetworkCredential(_UserName, _Password);
+            //команда фтп LIST
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            ftpRequest.EnableSsl = _UseSSL;
+            //Получаем входящий поток
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+
+            //переменная для хранения всей полученной информации
+            string content = "";
+
+            StreamReader sr = new StreamReader(ftpResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            content = sr.ReadToEnd();
+            sr.Close();
+            ftpResponse.Close();
+
+            DirectoryListParser parser = new DirectoryListParser(content);
+            return parser.FullListing;
+        }
+
+        //метод протокола FTP RETR для загрузки файла с FTP-сервера
+        public void DownloadFile(string path, string fileName)
+        {
+
+            ftpRequest = (FtpWebRequest)WebRequest.Create("ftp://" + _Host + path + "/" + fileName);
+
+            ftpRequest.Credentials = new NetworkCredential(_UserName, _Password);
+            //команда фтп RETR
+            ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            ftpRequest.EnableSsl = _UseSSL;
+            //Файлы будут копироваться в кталог программы
+            FileStream downloadedFile = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
+
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            //Получаем входящий поток
+            Stream responseStream = ftpResponse.GetResponseStream();
+
+            //Буфер для считываемых данных
+            byte[] buffer = new byte[1024];
+            int size = 0;
+
+            while ((size = responseStream.Read(buffer, 0, 1024)) > 0)
+            {
+                downloadedFile.Write(buffer, 0, size);
+
+            }
+            ftpResponse.Close();
+            downloadedFile.Close();
+            responseStream.Close();
+        }
+        
+    }
+}
---
+++ FTPClient/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPClient
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+            // create FtpWebRequest
+            //FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://10.7.180.107:21/regex.pdf");
+            //request.Method = WebRequestMethods.Ftp.DownloadFile;
+            //request.Credentials = new NetworkCredential("test_user", "1234567890");
+            //request.EnableSsl = true; // если используется ssl
+
+            // get answer as FtpWebResponse
+            //FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            //// get stream and save as file 
+            //Stream responseStream = response.GetResponseStream();
+            //FileStream fs = new FileStream("ph.jpg", FileMode.Create);
+            //byte[] buffer = new byte[64];
+            //int size = 0;
+
+            //while ((size = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            //{
+            //    fs.Write(buffer, 0, size);
+            //}
+            //fs.Close();
+            //response.Close();
+            Client client = new Client();
+            string pach = "";
+            int ie = 0;
+            while (true)
+            {
+                FileStruct[] Tmp = client.ListDirectory(pach);
+                Console.Clear();
+                for (int i = 0; i < Tmp.Length;i++)
+                {
+                    
+                    if (i == ie)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        if (Tmp[i].IsDirectory)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        if (Tmp[i].IsDirectory)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                    Console.WriteLine($"{Tmp[i].Name}");// | {item.Flags} | {item.CreateTime} \n");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                //Console.ReadKey();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        ie = (ie > 0 && ie < Tmp.Length) ? ie - 1 : Tmp.Length-1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        ie = (ie != Tmp.Length-1) ? ie + 1 : 0;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (Tmp[ie].IsDirectory)
+                        {
+                            pach += "/" + Tmp[ie].Name;
+                            ie = 0;
+                        }
+                        else
+                        {
+                            client.DownloadFile(pach, Tmp[ie].Name);
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (pach.Length > 0 || pach != null)
+                        {
+                            string[] authorsList = pach.Split('/');
+                            pach = "";
+                            for (int i = 0; i < authorsList.Length-1; i++)
+                            {
+                                pach += "/" + authorsList[i];
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+
+
+            //Console.WriteLine("Saving complete");
+            Console.Read();
+        }
+
+
+
+        
+    }
+}
